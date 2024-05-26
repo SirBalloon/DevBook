894f9ea19326cc2e866207a2beb090e76fb8183e
Kay._.Toyo <Kibrahim17@hotmail.com>
2024-05-26 13:03:12 +1000

    test

--- Frontend/src/Projects/Robotics/RoboticsAssignment.js
+++ Frontend/src/Projects/Robotics/RoboticsAssignment.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import ProductDOC from "../../Reusable_components/ProductDOC";
-import "../../CSS/LeetContents.css";
 import "../../CSS/ProjectContent.css";
+import "../../CSS/Body.css"
+import "../../CSS/GoBackButton.css"
 
 const RoboAssignment = () => {
   const navigate = useNavigate();
@@ -16,18 +16,7 @@ const RoboAssignment = () => {
       <button className="GoBackButton" onClick={handleGoBack}>
         Go Back
       </button>
-      <article className={`Projectarticle`}>
-        <div className="ProjectLine"></div>
-        <ProductDOC
-          title={"LeetCode"}
-          functionality={
-            "Starting 2024, I have made it my mission to become the best programmer I can be. One of my approaches to achieving this is to do atleast 2 leetcode problems a day. This project started on the 8th of January and will start being documented from the 20th of January. "
-          }
-          goals={
-            "My goal is to solve and break down the logic used behind the solution"
-          }
-        />
-        </article>
+      <p>hi</p>
     </div>
   );
 };
