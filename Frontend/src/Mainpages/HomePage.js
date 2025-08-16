3cfe56c0e1b6aa16b2619e08131a1cb0e926b7ac
Kay._.Toyo <Kibrahim17@hotmail.com>
2025-08-16 23:29:51 +1000

    Fixed SideBar with new design

--- Frontend/src/Mainpages/HomePage.js
+++ Frontend/src/Mainpages/HomePage.js
@@ -1,21 +1,19 @@
 import React from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 // import Header from "../Reusable_components/Header";
-// import Header2 from "../Reusable_components/Header(2)";
+import Header2 from "../Reusable_components/Header(2)";
 import HomeContent from "../Reusable_components/HomeBody";
 // import DevBookProject from "./DevBook";
 import LeetJourney from "../Projects/Leetcode/Leetcode";
 import CapstoneProject from "../Projects/CapstoneProj/CapstoneProject";
 import ElectronicsProg from "../Projects/ElectronicProg/ElectronicsProg";
-// import SliderLine from "../Reusable_components/SliderLine";
 import ResumeViewer from "../Reusable_components/ResumeViewer";
 
 const HomePage = () => {
   return (
     <Router>
       <div className="FlexDiv">
-          {/* <Header2 /> */}
-          {/* <SliderLine/> */}
+        <Header2 />
         <div className="BodyDiv">
           <Routes>
             <Route path="/" element={<HomeContent/>} />
--- Frontend/src/Reusable_components/Header(2).js
+++ Frontend/src/Reusable_components/Header(2).js
@@ -1,104 +1,71 @@
 import React from "react";
-import { useState } from "react";
 // import logo from "../images/catlogo.svg"
 import GitHub from "../images/github.svg";
 import LinkedIn from "../images/linkedin.svg";
 import Pinterest from "../images/pinterest.svg";
-import ResumeViewer from "./ResumeViewer";
+import Resume from "../images/resume.svg";
 import "../CSS/Header2.css";
 import "../CSS/Navbar.css";
 
 const Header2 = () => {
-  const [showResume, setShowResume] = useState(false);
-
   return (
-    <>
     <div className="headerContainer">
-      <section className="TEMPSection"></section>
-      {/* <div className="PFPimg">
-        <svg
-          width="50"
-          height="50"
-          viewBox="0 0 50 50"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <image href={logo} width="50" height="50" />
-        </svg>
-      </div> */}
-      <div className="hDivContainer">
-        <h1>@</h1>
-        <p className="hText">DevBook</p>
-        <p className="pText">by KI</p>
-      </div>
-      {/* <div className="NAVDivContainer"> */}
-        <nav>
-          <ul>
-            <li>
-              <a href="https://github.com/SirBalloon" target="GitHub" rel="noopener noreferrer">
-                <button className="socialsButton">
-                  <svg
-                    width="30"
-                    height="30"
-                    viewBox="0 0 30 30"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <image
-                      href={GitHub}
-                      width="30"
-                      height="30"
-                      className="img"
-                    />
-                  </svg>
+      <div
+      style={{
+        width: "100%",
+        height: "6%",
+        backgroundColor: "#ccc",
+      }}
+      ></div>
+      <nav>
+        <ul>
+          <li>
+            <a href="https://github.com/SirBalloon" target="GitHub" rel="noopener noreferrer">
+              <button className="socialsButton">
+                  <img
+                    src={GitHub}
+                    alt="Github"
+                    className="Icon"
+                  />
+              </button>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/khalil-ibrahim-774734260/" target="LinkedIn" rel="noopener noreferrer" >
+              <button className="socialsButton">
+                <img
+                  src={LinkedIn}
+                  alt="LinkedIn"
+                  className="Icon"
+                />
+              </button>
+            </a>
+          </li>
+          <li>
+            <a href="https://pin.it/2TQIhkPm8" target="Pinterest" rel="noopener noreferrer">
+              <button className="socialsButton">
+                <img
+                  src={Pinterest}
+                  alt="Pinterest"
+                  className="Icon"
+                />
+              </button>
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/SirBalloon" target="GitHub" rel="noopener noreferrer">
+              <button className="socialsButton">
+                  <img
+                    src={Resume}
+                    alt="Resume"
+                    className="Icon"
+                  />
                 </button>
-              </a>
-            </li>
-            <li>
-              <a href="https://www.linkedin.com/in/khalil-ibrahim-774734260/" target="LinkedIn" rel="noopener noreferrer" >
-                <button className="socialsButton">
-                  <svg
-                    width="30"
-                    height="30"
-                    viewBox="0 0 30 30"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <image
-                      href={LinkedIn}
-                      width="30"
-                      height="30"
-                      className="img"
-                    />
-                  </svg>
-                </button>
-              </a>
-            </li>
-            <li>
-              <a href="https://pin.it/2TQIhkPm8" target="Pinterest" rel="noopener noreferrer">
-                <button className="socialsButton">
-                  <svg
-                    width="30"
-                    height="30"
-                    viewBox="0 0 30 30"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <image
-                      href={Pinterest}
-                      width="30"
-                      height="30"
-                      className="img"
-                    />
-                  </svg>
-                </button>
-              </a>
-            </li>
-          </ul>
-        </nav>
-      {/* </div> */}
-      <div className="ResumeDivContainer">
-          <button className="resumeButton" onClick={() => setShowResume(true)}>Resume</button>
-      </div>
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
-    {showResume && <ResumeViewer onClose={() => setShowResume(false)} />}
-    </>
   );
 };
 
