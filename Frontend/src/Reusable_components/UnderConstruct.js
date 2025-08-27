2ff33b0c092f6adc47e365eb85b3630863ab2e67
Kay._.Toyo <Kibrahim17@hotmail.com>
2025-08-28 01:10:21 +1000

    New Construction sign

--- Frontend/src/Reusable_components/UnderConstruct.js
+++ Frontend/src/Reusable_components/UnderConstruct.js
@@ -5,8 +5,8 @@ const Construction = () => {
     return (
         <div className="ConstructionDiv">
             <img src={ConstructionSign} alt="construction" className="ConstructIcon"/>
-            <h1 className="Message">UNDER CONSTRUCTION</h1>
-            <div className="Message">
+            <h1 className="MessageHeader">UNDER CONSTRUCTION</h1>
+            <div className="MessageSubtitle">
                 <p>To be released soon.</p>
             </div>
         </div>
