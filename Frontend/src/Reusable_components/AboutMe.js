235f9a72d7b2b27c1538f93824f9d4ae2af42666
Kay._.Toyo <Kibrahim17@hotmail.com>
2026-01-09 01:15:46 +1100

    Newly Fixed HeroPage

---
+++ Frontend/src/Reusable_components/AboutMe.js
@@ -0,0 +1,21 @@
+import { motion } from "motion/react";
+import "./AboutMe.css";
+
+const AboutLayout = () => {
+
+  return (
+    <div className="about-container">
+        <motion.div className="about-left-column">
+            <h3>Welcome to my portfolio!</h3>
+            <p>It is a hub where anyone can see progress, work and documentation for all my projects. It serves to showcase my journey, open for interpretation and enjoyment! I have a background in Computer Science, whilst having taught myself other skills.</p>
+        </motion.div>
+        <motion.div className="about-right-column">
+            <p>"C, C++, Python, JavaScript(Frontend), React, TypeScript(Backend)</p>
+            <p>Continous updates to portfolio, Upskilling Myself in Embedded Systems and Electronics</p>
+            <p>Embedded Systems, Data Science and Web Development</p>
+        </motion.div>
+    </div>
+  );
+};
+
+export default AboutLayout;
