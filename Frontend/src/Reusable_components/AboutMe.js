cd025e5120af71aa698277ff88a505912d88fbad
Kay._.Toyo <Kibrahim17@hotmail.com>
2026-01-11 22:16:50 +1100

    Tweaked about me

--- Frontend/src/Reusable_components/AboutMe.js
+++ Frontend/src/Reusable_components/AboutMe.js
@@ -6,8 +6,8 @@ const AboutLayout = () => {
   return (
     <div className="about-container">
         <motion.div className="about-left-column">
-            <h3>Welcome to my portfolio!</h3>
-            <p>It is a hub where anyone can see progress, work and documentation for all my projects. It serves to showcase my journey, open for interpretation and enjoyment! I have a background in Computer Science, whilst having taught myself other skills.</p>
+            <h3>WELCOME TO MY PORTFOLIO!</h3>
+            <p>This is a hub where anyone can see progress, code, hardware and documentation for all my projects. It serves to showcase my journey, open for interpretation and enjoyment! I have a background in Computer Science, whilst having taught myself other skills such as soldering, electronics, circuits, and much more.</p>
             <h4>Interests - </h4>
             <p>Embedded Systems, Web Development and software development</p>
         </motion.div>
@@ -15,7 +15,7 @@ const AboutLayout = () => {
             <h4>Skills & Goals - </h4>
             <p>C, C++, Python, JavaScript(Frontend), React, TypeScript(Backend)</p>
             <h4>Current Projects - </h4>
-            <p>Continous updates to portfolio, Upskilling Myself in Embedded Systems and Electronics</p>
+            <p>Continous updates to my portfolio, Upskilling myself in Embedded Systems and Electronics</p>
         </motion.div>
     </div>
   );
