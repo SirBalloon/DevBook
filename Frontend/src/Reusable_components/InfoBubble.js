6c10df335aa52f335ca4abf1ea95254dd3bd4d6e
Kay._.Toyo <Kibrahim17@hotmail.com>
2026-01-12 10:30:25 +1100

    Fixed projects mobile view

--- Frontend/src/Reusable_components/InfoBubble.js
+++ Frontend/src/Reusable_components/InfoBubble.js
@@ -58,6 +58,7 @@ const InfoBubble = ({ InfoData, Layout, onReset }) => {
             initial={{ opacity: 0, y: -20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.3, duration: 0.8 }}
+            className="static-message"
             style={{
               position: 'absolute',
               top: '40%',
@@ -74,7 +75,8 @@ const InfoBubble = ({ InfoData, Layout, onReset }) => {
               maxWidth: '400px'
             }}
           >
-            Select a file on the left by clicking the title tag
+            <span className="desktop-message">Select a file on the left by clicking the title tag</span>
+            <span className="mobile-message">Select a file from above by clicking the title tags</span>
           </motion.div>
         </motion.div>
       ) : Layout === "AboutMe" ? (
